5b5f61aa9777ff54c94436df6a99cb9736c38336
Alejandro Giraldo Duque <alejogo4>
2021-01-06 09:20:09 -0500

    UI

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:petApp/screens/drawer_screen.dart';
+import 'package:petApp/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomePage(),
+      theme: ThemeData(fontFamily: 'Circular'),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        body: Stack(
+          children: [
+            DrawerScreen(),
+            HomeScreen(),
+          ],
+        ),
+      ),
+    );
+  }
+}
